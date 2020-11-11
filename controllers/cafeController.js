ec852f891ec30395ca7efd063343e0ceb2fe0a3a
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-11-11 11:50:40 -0800

    Added refresh database function and like route

--- controllers/cafeController.js
+++ controllers/cafeController.js
@@ -6,21 +6,36 @@ const mongoose = require("mongoose");
 const Cafe = require("../models/cafeModel")
 
 // Refresh a place id for one cafe
-async function refreshPlaceId(mongoId, placeId) {
+async function refreshDatabase() {
     try {
-        let newId = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=place_id&key=${process.env.API_Key}`)
-        result = await Cafe.findOneAndUpdate(
-            {
-                _id: mongoose.Types.ObjectId(req.params.id)
-            },
-            {
-                place_id: newId
-            })
-        console.log(result)
+        let cafeIds = await Cafe.find({}, '_id place_id')
+        for (cafeId of cafeIds) {
+            let place = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${cafeId.place_id}&fields=place_id,name,geometry/location/lat,geometry/location/lng,formatted_address,website,opening_hours/weekday_text,photos&key=${process.env.API_Key}`)
+            let result = await Cafe.findOneAndUpdate(
+                {
+                    _id: mongoose.Types.ObjectId(cafeId._id)
+                },
+                {
+                    place_id: place.data.result.place_id,
+                    name: place.data.result.name,
+                    lat: place.data.result.geometry.location.lat,
+                    lng: place.data.result.geometry.location.lng,
+                    address: place.data.result.formatted_address,
+                    website: place.data.result.website,
+                    weekday_text: place.data.result.opening_hours.weekday_text,
+                    photos: place.data.result.photos // Google stores a 'photo reference' instead of a url. Maybe we should convert before saving into database
+                })
+            console.log(`Updated ${cafeId._id}: ${result}`)
+        }
     } catch (err) {
         console.error(err)
     }
 }
+
+router.get("/api/refresh", async function (req, res) {
+    refreshDatabase();
+    res.send("Running refresh")
+})
 
 // Route to seed database with coffee shops within 500m of my house
 router.get("/api/seed", async function (req, res) {
@@ -29,7 +44,7 @@ router.get("/api/seed", async function (req, res) {
         let response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.649349,%20-122.321053&radius=${radius}&keyword=coffee&key=${process.env.API_Key}`)
         let placeIds = response.data.results.map(place => place.place_id)
         for (id of placeIds) {
-            let place = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&key=${process.env.API_Key}`)
+            let place = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&fields=place_id,name,geometry/location/lat,geometry/location/lng,formatted_address,website,opening_hours/weekday_text,photos&key=${process.env.API_Key}`)
             let placeObj = {
                 place_id: place.data.result.place_id,
                 name: place.data.result.name,
@@ -38,19 +53,24 @@ router.get("/api/seed", async function (req, res) {
                 address: place.data.result.formatted_address,
                 website: place.data.result.website,
                 weekday_text: place.data.result.opening_hours.weekday_text,
-                photos: place.data.result.photos // Google stores a 'photo reference' instead of a url. Maybe we should convert before saving into database
+                photos: place.data.result.photos, // Google stores a 'photo reference' instead of a url. Maybe we should convert before saving into database
+                custom_data: {
+                    roasters: [],
+                    photos: [],
+                    likes: 0
+                }
             }
             Cafe.create(placeObj)
         }
         res.redirect("/api/cafes")
     } catch (err) {
         console.error(err)
-        res.set(500).send("An Error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
 // Search Places API by cafe name
-router.get("/api/places/:cafename", async function (req, res) {
+router.get("/api/search/:cafename", async function (req, res) {
     try {
         let { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.params.cafename}&inputtype=textquery&key=${process.env.API_KEY}`)
         let candidates = data.candidates
@@ -62,7 +82,7 @@ router.get("/api/places/:cafename", async function (req, res) {
         res.json(places)
     } catch (err) {
         console.error(err);
-        res.set(500).send("An Error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
@@ -73,7 +93,7 @@ router.get("/api/cafes", async function (req, res) {
         res.json(result)
     } catch (err) {
         console.error(err)
-        res.set(500).send("An error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
@@ -84,7 +104,27 @@ router.get("/api/cafes/:id", async function (req, res) {
         res.json(result)
     } catch (err) {
         console.error(err)
-        res.set(500).send("An error!")
+        res.set(500).send("An error has appeared!")
+    }
+})
+
+router.get("/api/cafes/like/:id", async function (req, res) {
+    try {
+        let result = await Cafe.findOneAndUpdate(
+            {
+                _id: mongoose.Types.ObjectId(req.params.id)
+            },
+            {
+                $inc: { "custom_data.likes": 1 }
+            })
+        if (result) {
+            res.send(`_id:${req.params.id} updated`)
+        } else {
+            res.set(404).send(`_id:${req.params.id} not found`)
+        }
+    } catch (err) {
+        console.error(err);
+        res.set(500).send("An error has appeared!")
     }
 })
 
@@ -105,7 +145,7 @@ router.post("/api/cafes", async function (req, res) {
         res.json(result)
     } catch (err) {
         console.error(err)
-        res.set(500).send("An error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
@@ -126,7 +166,7 @@ router.put("/api/cafes/:id", async function (req, res) {
         res.json(updated)
     } catch (err) {
         console.error(err)
-        res.set(500).send("An error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
@@ -137,7 +177,7 @@ router.delete("/api/cafes/:id", async function (req, res) {
         res.json(result)
     } catch (err) {
         console.error(err)
-        res.set(500).send("An error!")
+        res.set(500).send("An error has appeared!")
     }
 })
 
