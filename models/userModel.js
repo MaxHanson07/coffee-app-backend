db4db535037e6eb7a6d64dc1df7012b1f2d7218f
Max Hanson <mthanson2@gmail.com>
2020-11-18 13:12:37 -0800

    Edited user model

--- models/userModel.js
+++ models/userModel.js
@@ -56,11 +56,11 @@ UserSchema.pre('save', function(next) {
     });
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
+// UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//         if (err) return cb(err);
+//         cb(null, isMatch);
+//     });
+// };
 
 module.exports = mongoose.model('User', UserSchema);
