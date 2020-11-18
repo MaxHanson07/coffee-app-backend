fc339132eec136ff3036a9061b484aafd363e67d
Max Hanson <mthanson2@gmail.com>
2020-11-18 12:46:47 -0800

    Added checkAuthStatus function

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,24 +4,24 @@ const db = require('../models')
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// const checkAuthStatus = request => {
-//     console.log(request.headers);
-//     if (!request.headers.authorization) {
-//         return false
-//     }
-//     const token = request.headers.authorization.split(" ")[1]
-//     console.log(token);
-//     const loggedInUser = jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
-//         if (err) {
-//             return false
-//         }
-//         else {
-//             return data
-//         }
-//     });
-//     console.log(loggedInUser)
-//     return loggedInUser
-// }
+const checkAuthStatus = request => {
+    console.log(request.headers);
+    if (!request.headers.authorization) {
+        return false
+    }
+    const token = request.headers.authorization.split(" ")[1]
+    console.log(token);
+    const loggedInUser = jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+        if (err) {
+            return false
+        }
+        else {
+            return data
+        }
+    });
+    console.log(loggedInUser)
+    return loggedInUser
+}
 
 router.get("/", (req, res) => {
     db.User.findAll().then(dbUsers => {
